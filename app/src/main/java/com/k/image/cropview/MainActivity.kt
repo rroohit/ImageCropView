27753618d2524961810786c01770e3335e72ae23
crohi <crohitp9@gmail.com>
2022-12-21 22:05:34 +0530

    Image-Crop-View

---
+++ app/src/main/java/com/k/image/cropview/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.k.image.cropview
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.k.image.cropview.ui.theme.ImageCropViewTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ImageCropViewTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
+
