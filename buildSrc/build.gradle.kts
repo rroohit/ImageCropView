5f33dc55d8f1fdcdb8ae4b5986e32056c5d8ec0e
rohitt <crohitp9@gmail.com>
2023-12-25 01:25:59 +0530

    release v_2.0.1 added crop-rect draggable inside the crop view.

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,6 +1,14 @@
 plugins {
     `kotlin-dsl`
 }
+
 repositories {
     mavenCentral()
 }
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
--- buildSrc/src/main/kotlin/com/github/rroohit/imagecropview/Configuration.kt
+++ buildSrc/src/main/kotlin/com/github/rroohit/imagecropview/Configuration.kt
@@ -4,9 +4,9 @@ object Configuration {
     const val compileSdk = 34
     const val targetSdk = 34
     const val minSdk = 21
-    const val majorVersion = 2
-    const val minorVersion = 0
-    const val patchVersion = 1
+    private const val majorVersion = 2
+    private const val minorVersion = 0
+    private const val patchVersion = 1
     const val versionName = "$majorVersion.$minorVersion.$patchVersion"
     const val versionCode = 3
     const val snapshotVersionName = "$majorVersion.$minorVersion.${patchVersion + 1}-SNAPSHOT"
