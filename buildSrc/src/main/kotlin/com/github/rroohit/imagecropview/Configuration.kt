8d7e2180d096119b241346f4c828c2cbf574a712
rohitt <crohitp9@gmail.com>
2024-01-10 22:29:11 +0530

    Implemented drag functionality for crop rectangle

--- buildSrc/src/main/kotlin/com/github/rroohit/imagecropview/Configuration.kt
+++ buildSrc/src/main/kotlin/com/github/rroohit/imagecropview/Configuration.kt
@@ -4,11 +4,12 @@ object Configuration {
     const val compileSdk = 34
     const val targetSdk = 34
     const val minSdk = 21
-    private const val majorVersion = 2
-    private const val minorVersion = 0
-    private const val patchVersion = 1
+    const val minSdkBenchmark = 23
+    const val majorVersion = 2
+    const val minorVersion = 1
+    const val patchVersion = 0
     const val versionName = "$majorVersion.$minorVersion.$patchVersion"
-    const val versionCode = 3
+    const val versionCode = 4
     const val snapshotVersionName = "$majorVersion.$minorVersion.${patchVersion + 1}-SNAPSHOT"
-    const val artifactGroup = "com.github.rroohit"
+    const val artifactGroup = "io.github.rroohit"
 }
