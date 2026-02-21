0e51506fb88219978b6cf49e482aad9e890c0cbe
charlie <crohitp9@gmail.com>
2026-02-21 18:05:11 +0530

    Comment out Java toolchain configuration in `buildSrc`

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,11 +7,7 @@ repositories {
 }
 
 java {
-    /*toolchain {
-         //languageVersion.set(JavaLanguageVersion.of(24))
-         languageVersion.set(JavaLanguageVersion.of(21))
 
-    }*/
 }
 
 
